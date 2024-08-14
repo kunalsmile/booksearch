2f26f24ae0f0f7cba826eabc02baad8ce24141cd
kunalsmile <kunalsmile@gmail.com>
2024-08-14 11:38:47 +0530

    Update GApiBook.py

--- GApiBook.py
+++ GApiBook.py
@@ -24,7 +24,7 @@ def prepareList():
 
 
 def getSearchResult():
-    return requests.get("https://www.googleapis.com/books/v1/volumes?q=" + st.session_state.keyword).json()
+    return requests.get("https://www.googleapis.com/books/v1/volumes?q=" + st.session_state.keyword + "&countryCode=NONE").json()
 
 
 def getSelectedCheckboxes():
